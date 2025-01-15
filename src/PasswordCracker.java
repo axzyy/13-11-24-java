e286c6dc283a6cef268d14c8deb491b7d83636eb
axzyy <99994889+axzyy@users.noreply.github.com>
2025-01-15 09:46:12 +0000

    multithread

--- src/PasswordCracker.java
+++ src/PasswordCracker.java
@@ -1,6 +1,6 @@
 
 
-public class PasswordCracker {
+public class PasswordCracker extends Thread{
 
 	// Un oggetto di questa classe rappresenta un hacker che cerca di violare
 	// la password segreta per aprire il file protetto che viene ricevuto nel
@@ -9,14 +9,16 @@ public class PasswordCracker {
 	private PasswordGenerator passwordGenerator;
 	private PasswordProtectedFile fileToCrack;
 	private int password_length;
+	private int numero;
 
-	public PasswordCracker(PasswordProtectedFile file, int password_length) {
+	public PasswordCracker(PasswordProtectedFile file, int password_length, int numero) {
 		this.password_length = password_length;
 		this.passwordGenerator = new PasswordGenerator();
 		this.fileToCrack = file;
+		this.numero =numero;
 	}
 
-	public String crack_it() {
+	private String crack_it() {
 		// TODO: continua a generaqre password tramite un generatore
 		// fino a quando non ottiene la corrispondenza con la password
 		// del file. A questo punto ritorna la password al chiamante.
@@ -39,6 +41,14 @@ public class PasswordCracker {
 
 		return password;
 
+	}
+	public  void run(){
+		Cronometro crono = new Cronometro();
+		crono.start();
+		String secret_password = crack_it();
+		System.out.println("La password segreta: " + secret_password);
+		crono.stop();
+		System.out.println("e' stata violata in: " + crono.elapsed() + " millisecondi" + " " +  this.numero);
 	}
 
 }
